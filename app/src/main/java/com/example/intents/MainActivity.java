24e41e3b31ef0aa4f5c0268360cdea88cdf72935
HP <kyathisri20@gmail.com>
2022-05-06 06:08:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/intents/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.intents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText e1;
+    Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        e1=(EditText) findViewById(R.id.edit);
+        b1=(Button) findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str=e1.getText().toString();
+                Intent intent=new Intent(getApplicationContext(),MainActivity2.class);
+                intent.putExtra("message_key",str);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/intents/MainActivity2.java
@@ -0,0 +1,20 @@
+package com.example.intents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+TextView t1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        t1=(TextView) findViewById(R.id.text);
+        Intent intent=getIntent();
+       String str= intent.getStringExtra("message_key");
+        t1.setText(str);
+    }
+}
